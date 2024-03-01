a917eccdacdd0d27d44b2a8517fcad000fb825a6
ericalundstrom <erica.lundstrom99@hotmail.com>
2024-03-01 11:35:42 +0100

    update

--- index.js
+++ index.js
@@ -27,15 +27,24 @@ async function CreateButtons() {
         ButtonDom.textContent = text;
         ButtonWrapper.append(ButtonDom);
         ButtonDom.addEventListener("click", (e) => {
-            filterDataViz(e);
+            filterDataViz(e, d);
         });
     });
 }
 
 CreateButtons();
 
-function filterDataViz(e) {
-    console.log(e.target.textContent);
+function filterDataViz(e, key) {
+    console.log(e);
+    console.log(key);
+    let svg = document.querySelector("svg")
+    if (svg) {
+        svg.remove()
+        CreateBubbles(key)
+    } else {
+
+        CreateBubbles(key)
+    }
 }
 
 let wSvg = 1000;
@@ -44,24 +53,71 @@ let hViz = 0.9 * hSvg;
 let wViz = 0.9 * wSvg;
 let wPadding = (wSvg - wViz) / 2;
 let hPadding = (hSvg - hViz) / 2;
-
-let svg = d3
-    .select("body")
-    .append("svg")
-    .attr("height", hSvg)
-    .attr("width", wSvg);
+let margin = 1;
 
 
 
 
-fetching().then(data => {
-    // Extract keys from the JSON object
-    let keys = Object.keys(data);
-    let gViz = svg.append("g")
-        .selectAll()
-        .data(keys)
-        .enter()
-        .append("circle")
+async function CreateBubbles(key) {
 
-    console.log(gViz);
-});
+    let svg = d3
+        .select("body")
+        .append("svg")
+        .attr("height", hSvg)
+        .attr("width", wSvg);
+
+
+
+    const pack = d3
+        .pack()
+        .size([wViz - margin * 2, hViz - margin * 2])
+        .padding(3);
+
+    const bigDataset = await fetching();
+
+    const root = pack(
+        d3.hierarchy({ children: bigDataset }).sum((d) => d[key])
+    ); // Use the selected property for sizing
+
+    fetching().then(data => {
+        // Extract keys from the JSON object
+        let keys = Object.keys(data);
+        let gViz = svg.append("g")
+            .selectAll()
+            .data(root.leaves())
+            .enter()
+            .append("g")
+            // .attr("transform", d => `translate(${Randomize()[0] * 6.5}, ${Randomize()[1] * 6.5})`)
+            .attr("transform", d => `translate(${d.x},${d.y})`)
+            .append("circle")
+            .attr("r", 10)
+
+
+        console.log(gViz);
+    });
+
+    // let gViz = svg.append("g")
+    //     .selectAll()
+    //     .data(root.leaves())
+    //     .enter()
+    //     .append("g")
+    //     .attr("transform", d => `translate(${Randomize()[0] * 6.5}, ${Randomize()[1] * 6.5})`)
+    //     .append("circle")
+    //     .attr("r", 10)
+
+}
+
+
+
+function Randomize() {
+    let x = [10, 20, 30, 40, 50, 60];
+    let y = [50, 10, 40, 70, 60, 30, 20];
+
+    // Randomly select an index for array x
+    let randomIndexX = Math.floor(Math.random() * x.length);
+    // Randomly select an index for array y
+    let randomIndexY = Math.floor(Math.random() * y.length);
+
+    // Return an array with the randomly selected values from x and y
+    return [x[randomIndexX], y[randomIndexY]];
+}
