b2fbce10eee07cf7506636861f8f47544e9804c5
Oliwer <oliwerl@outlook.com>
2024-03-05 09:32:58 +0100

    update

--- index.js
+++ index.js
@@ -1,50 +1,46 @@
-
-
 async function fetching() {
-    const data = await d3.json("dataset/healthy_lifestyle_city_2021.json");
-    return data;
+  const data = await d3.json("dataset/healthy_lifestyle_city_2021.json");
+  return data;
 }
 
 async function CreateButtons() {
-    let keys = [];
-    let bigDataset = await fetching();
+  let keys = [];
+  let bigDataset = await fetching();
 
-    let firstData = bigDataset[0];
+  let firstData = bigDataset[0];
 
-    for (const key in firstData) {
-        if (key === "City" || key === "flag" || key === "Rank") {
-
-        } else {
-            keys.push(key);
-        }
+  for (const key in firstData) {
+    if (key === "City" || key === "flag" || key === "Rank") {
+    } else {
+      keys.push(key);
     }
-    let ButtonWrapper = document.createElement("div");
-    ButtonWrapper.classList.add("ButtonBox");
-    document.querySelector("#wrapper").append(ButtonWrapper);
-    keys.forEach((d) => {
-        let ButtonDom = document.createElement("button");
-        ButtonDom.classList.add("button");
-        let text = d.replace(/_/g, ' ');
-        ButtonDom.textContent = text;
-        ButtonWrapper.append(ButtonDom);
-        ButtonDom.addEventListener("click", (e) => {
-            filterDataViz(e, d);
-        });
+  }
+  let ButtonWrapper = document.createElement("div");
+  ButtonWrapper.classList.add("ButtonBox");
+  document.querySelector("#wrapper").append(ButtonWrapper);
+  keys.forEach((d) => {
+    let ButtonDom = document.createElement("button");
+    ButtonDom.classList.add("button");
+    let text = d.replace(/_/g, " ");
+    ButtonDom.textContent = text;
+    ButtonWrapper.append(ButtonDom);
+    ButtonDom.addEventListener("click", (e) => {
+      filterDataViz(e, d);
     });
+  });
 }
 
 function filterDataViz(e, key) {
-    let svg = document.querySelector("svg");
-    if (svg) {
-        svg.remove();
-        CreateBubbles(key);
-    } else {
-        CreateBubbles(key);
-    }
+  let svg = document.querySelector("svg");
+  if (svg) {
+    svg.remove();
+    CreateBubbles(key);
+  } else {
+    CreateBubbles(key);
+  }
 }
 
-
-let wSvg = 1000;
+let wSvg = 800;
 let hSvg = 800;
 let hViz = 0.9 * hSvg;
 let wViz = 0.9 * wSvg;
@@ -53,67 +49,64 @@ let hPadding = (hSvg - hViz) / 2;
 let margin = 1;
 
 let gViz;
+let view;
 
 async function CreateBubbles(key) {
-    const bigDataset = await fetching();
+  const bigDataset = await fetching();
 
-    let svg = d3
-        .select("#wrapper")
-        .append("svg")
-        .attr("height", hSvg)
-        .attr("width", wSvg);
+  let svg = d3
+    .select("#wrapper")
+    .append("svg")
+    .attr("height", hSvg)
+    .attr("width", wSvg);
 
+  const pack = d3
+    .pack()
+    .size([wViz - margin * 2, hViz - margin * 2])
+    .padding(3);
 
-    const pack = d3
-        .pack()
-        .size([wViz - margin * 2, hViz - margin * 2])
-        .padding(10);
+  const root = pack(
+    d3
+      .hierarchy({ children: bigDataset })
+      .sum((d) => (!isNaN(d[key]) ? d[key] : 1))
+  );
 
-    const root = pack(
-        d3
-            .hierarchy({ children: bigDataset })
-            .sum((d) => (!isNaN(d[key]) ? d[key] : 1))
+  const radiusScale = d3
+    .scaleLinear()
+    .domain([
+      d3.min(root.leaves(), (d) => d.r),
+      d3.max(root.leaves(), (d) => d.r),
+    ])
+    .range([10, 40]);
+
+  gViz = svg // Assign gViz here
+    .append("g")
+    .selectAll()
+    .data(root.leaves())
+    .enter()
+    .append("g")
+    .attr("transform", (d) => `translate(${d.x},${d.y})`)
+    .on(
+      "click",
+      (event, d) => focus !== d && (zoom(event, d), event.stopPropagation())
     );
 
-    const radiusScale = d3
-        .scaleLinear()
-        .domain([
-            d3.min(root.leaves(), (d) => d.r),
-            d3.max(root.leaves(), (d) => d.r),
-        ])
-        .range([10, 50]);
+  // Append circle with dynamic radius
+  gViz
+    .append("circle")
+    .attr("r", (d) => radiusScale(d.r))
+    .style("fill", "none");
 
-
-
-    gViz = svg // Assign gViz here
-        .append("g")
-        .selectAll()
-        .data(root.leaves())
-        .enter()
-        .append("g")
-        .attr("transform", (d) => `translate(${d.x},${d.y})`)
-        .on(
-            "click",
-            (event, d) => focus !== d && (zoom(event, d), event.stopPropagation())
-        );
-
-    // Append circle with dynamic radius
-    gViz
-        .append("circle")
-        .attr("r", (d) => radiusScale(d.r))
-        .style("fill", "none");
-
-    // Append foreignObject with the same size as the circle and hover effect
-    gViz
-        .append("foreignObject")
-        .attr("width", (d) => radiusScale(d.r) * 2)
-        .attr("height", (d) => radiusScale(d.r) * 2)
-        .attr("x", (d) => -radiusScale(d.r))
-        .attr("y", (d) => -radiusScale(d.r))
-        .html(
-            (d) =>
-
-                `<div class="flag-image" style="background-image: url(${d.data.flag});">
+  // Append foreignObject with the same size as the circle and hover effect
+  gViz
+    .append("foreignObject")
+    .attr("width", (d) => radiusScale(d.r) * 2)
+    .attr("height", (d) => radiusScale(d.r) * 2)
+    .attr("x", (d) => -radiusScale(d.r))
+    .attr("y", (d) => -radiusScale(d.r))
+    .html(
+      (d) =>
+        `<div class="flag-image" style="background-image: url(${d.data.flag});">
                     <div id="info" style="opacity: 0">
                        <p id="title"> ${d.data.City} </p>
                         <div id="Rank"> 
@@ -163,99 +156,96 @@ async function CreateBubbles(key) {
 
                     </div>
                 </div>`
+    )
+    // .select("flag-image > p").style("opacity", 0)
+    .on("mouseover", function (event, d) {
+      // Show tooltip on hover
+      tooltip.style("opacity", 0.9);
+    })
+    .on("mousemove", function (event, d) {
+      // Move tooltip to follow the mouse
+      let text = key.replace(/_/g, " ");
 
-        )
-        // .select("flag-image > p").style("opacity", 0)
-        .on("mouseover", function (event, d) {
-            // Show tooltip on hover
-            tooltip.style("opacity", 0.9);
-        })
-        .on("mousemove", function (event, d) {
-            // Move tooltip to follow the mouse
-            let text = key.replace(/_/g, ' ')
+      tooltip
+        .html(` <b>${d.data.City}</b>, ${text}: ${d.data[key]}`)
+        .style("left", event.pageX + 10 + "px")
+        .style("top", event.pageY - 28 + "px");
+    })
+    .on("mouseout", function (event, d) {
+      // Hide tooltip on mouseout
+      tooltip.style("opacity", 0);
+    })
+    .on("mouseenter", function (event, d) {
+      let foreign = d3.select(this);
+      foreign.transition().style("transform", "scale(1.5)");
 
-            tooltip
-                .html(` <b>${d.data.City}</b>, ${text}: ${d.data[key]}`)
-                .style("left", event.pageX + 10 + "px")
-                .style("top", event.pageY - 28 + "px");
-        })
-        .on("mouseout", function (event, d) {
-            // Hide tooltip on mouseout
-            tooltip.style("opacity", 0);
-        })
-        .on("mouseenter", function (event, d) {
-            let foreign = d3.select(this);
-            foreign.transition()
-                .style("transform", "scale(1.5)")
+      let flagImage = d3.select(this);
+      flagImage
+        .style("display", "flex")
+        .style("justify-content", "center")
+        .style("align-items", "center");
+      // .style("z-index", 1)
+    })
+    .on("mouseleave", function (event, d) {
+      let foreign = d3.select(this);
+      foreign.transition().style("transform", "scale(1)");
+      d3.select(this).select(".flag-image");
+    });
 
-            let flagImage = d3.select(this);
-            flagImage.style("display", "flex")
-                .style("justify-content", "center")
-                .style("align-items", "center")
-            // .style("z-index", 1)
+  // Create a tooltip
+  let tooltip = d3
+    .select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
-        })
-        .on("mouseleave", function (event, d) {
-            let foreign = d3.select(this);
-            foreign.transition().style("transform", "scale(1)")
-            d3.select(this)
-                .select(".flag-image")
+  let focus = root;
+  let view;
 
-        })
+  svg.on("click", (event) => {
+    zoom(event, root);
+  });
 
+  zoomTo([focus.x, focus.y, focus.r * 2]);
 
-    // Create a tooltip
-    let tooltip = d3
-        .select("body")
-        .append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
+  function zoomTo(v, e) {
+    const k = wSvg / v[2];
 
+    const centerX = wSvg / 2;
+    const centerY = hSvg / 2 - 30;
+    view = v;
 
-    let focus = root;
-    let view;
+    gViz.attr(
+      "transform",
+      (d) =>
+        `translate(${(d.x - v[0]) * k + centerX},${(d.y - v[1]) * k + centerY})`
+    );
+    gViz.select("circle").attr("r", (d) => radiusScale(d.r) * k);
+    gViz
+      .select("foreignObject")
+      .attr("width", (d) => radiusScale(d.r) * 2 * k)
+      .attr("height", (d) => radiusScale(d.r) * 2 * k)
+      .attr("x", (d) => -radiusScale(d.r) * k)
+      .attr("y", (d) => -radiusScale(d.r) * k);
+  }
 
-    svg.on("click", (event) => { zoom(event, root) });
+  function zoom(event, d) {
+    focus = d;
+    (d) => (!isNaN(d[key]) ? d[key] : 1);
 
-    zoomTo([focus.x, focus.y, focus.r * 2]);
+    const transition = svg
+      .transition() // Use the passed SVG element directly
+      .duration(event.altKey ? 7500 : 750)
+      .tween("zoom", () => {
+        const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2.5]);
+        return (t) => zoomTo(i(t), d);
+      });
 
+    let chosen = event.target.offsetParent.children[0].children[0];
+    chosen.classList.toggle("chosen");
 
-    function zoomTo(v, e) {
-        const k = wSvg / v[2];
-
-        const centerX = wSvg / 2;
-        const centerY = hSvg / 2 - 30;
-        view = v;
-
-        gViz.attr("transform", d => `translate(${(d.x - v[0]) * k + centerX},${(d.y - v[1]) * k + centerY})`);
-        gViz.select("circle").attr("r", d => radiusScale(d.r) * k);
-        gViz.select("foreignObject")
-            .attr("width", (d) => radiusScale(d.r) * 2 * k)
-            .attr("height", (d) => radiusScale(d.r) * 2 * k)
-            .attr("x", (d) => -radiusScale(d.r) * k)
-            .attr("y", (d) => -radiusScale(d.r) * k)
-
-    }
-
-
-    function zoom(event, d) {
-        focus = d;
-        (d) => (!isNaN(d[key]) ? d[key] : 1)
-
-        const transition = svg.transition() // Use the passed SVG element directly
-            .duration(event.altKey ? 7500 : 750)
-            .tween("zoom", () => {
-                const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2.5]);
-                return t => zoomTo(i(t), d);
-            });
-
-        let chosen = event.target.offsetParent.children[0].children[0]
-        chosen.classList.toggle("chosen");
-
-        chosen.style.opacity = chosen.classList.contains("chosen") ? "1" : "0";
-    }
-
-
+    chosen.style.opacity = chosen.classList.contains("chosen") ? "1" : "0";
+  }
 }
 
 CreateButtons();
