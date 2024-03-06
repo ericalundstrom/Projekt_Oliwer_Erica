c912e14456134cf5e16a9dc4e194b8c45c702a92
ericalundstrom <erica.lundstrom99@hotmail.com>
2024-03-06 21:41:30 +0100

    upd

--- EricasWish.js
+++ EricasWish.js
@@ -3,6 +3,31 @@ async function fetching() {
     return data;
 }
 
+function basicLayout(params) {
+    let wrapper = document.querySelector("#wrapper");
+    wrapper.innerHTML = `
+
+    <div id="background">
+        <h1> Healthy lifestyle around the globe </h1>
+
+        <p>According to a study by YouGov, almost half of Brits stated improving fitness (47%), losing weight (44%), and improving diet (41%) were among their most important New Year’s resolutions for 2020. It’s no surprise that millions of people worldwide enter each New Year with the hope of taking better care of themselves, whether that’s signing up for the gym or eating healthier.</p>
+        <p> However, fitness and diet are not the only measurements of healthiness - although they do play a big part. Sometimes it’s about our surrounding environment, as well as lifestyle choices.</p>
+        <p> The team at Lenstore has analyzed 44 cities across the globe to uncover where it’s easier to lead a well-rounded, healthy lifestyle. From obesity levels to pollution rates, each city has been scored across 10 healthy living metrics.</p>
+        <p> By analyzing factors such as obesity levels, pollution rates, access to outdoor activities, and more, we aim to provide users with a comprehensive view of what it means to live healthily in different urban environments. This data-driven approach allows individuals to make more informed decisions about where they live and how they can improve their quality of life.  </p>
+        <p> The team at Lenstore is dedicated to promoting health and well-being, and we believe that by shedding light on these important metrics, we can empower individuals to make positive changes for themselves and their communities. </p>
+        <p> As two students at Malmö Universitet, we were intrigued by Lenstore's analysis and decided to delve deeper into the data. Our goal is to create a visualization that provides users with valuable insights into the factors that contribute to a healthy lifestyle in different urban environments. </p>
+        <p> By combining our passion for data analysis with our commitment to promoting health and well-being, we aim to empower individuals to make informed decisions about their living arrangements and lifestyle choices. Join us on this journey as we explore the pathways to a healthier and more fulfilling life. </p>
+        <br>
+        <p id="finalQuote"> One question remains, do <b> you</b> live in the best city for healthy living? </p>
+        <br>
+
+    </div>
+    `;
+
+    document.querySelector("footer").textContent = "© This data is provided by Kaggle.com. Made my Oliwer Löfgren and Erica Lundström ©";
+    CreateButtons();
+}
+
 async function CreateButtons() {
     let keys = [];
     let bigDataset = await fetching();
@@ -135,14 +160,14 @@ async function CreateBubbles(key, value) {
 
     // Update the text content with a transition effect
     range.transition()
-        .duration(500) // Adjust the duration as needed
+        .duration(300) // Adjust the duration as needed
         .tween("text", function () {
             // Interpolate between the current text content and the new content
-            const interpolate = d3.interpolate(this.textContent, `Min: ${minValue} Max: ${maxValue}`);
+            const interpolate = d3.interpolate(this.textContent, `Min value: <br> ${minValue}<br> <br>Max value: <br> ${maxValue}`);
 
             // Return a function that updates the text content gradually
             return function (t) {
-                this.textContent = interpolate(t);
+                this.innerHTML = interpolate(t);
             };
         });
 
@@ -157,6 +182,9 @@ async function CreateBubbles(key, value) {
         let x = (h / 2) + xaxis;
         let y = (w / 2) + yaxis
         // console.log(xPad, Pad);
+
+        // let x = (index % n_cols) * w;
+        // let y = Math.floor(index / n_cols) * h;
         return { x, y };
     }
 
@@ -174,14 +202,35 @@ async function CreateBubbles(key, value) {
         .range([60, 0.8 * w]);
 
 
-    if (value) {
+    var linearSize = d3.scaleLinear().domain([0, maxValue]).range([10, 30]);
 
+    svg.append("g")
+        .attr("class", "legendSize")
+        .attr("transform", `translate(${(wViz - wPadding) / 2}, 20)`);
+
+    var legendSize = d3.legendSize()
+        .scale(linearSize)
+        .shape('circle')
+        .shapePadding(15)
+        .labelOffset(20)
+        .orient('horizontal')
+
+    svg.select(".legendSize")
+        .call(legendSize)
+        .transition();
+
+
+
+
+
+    if (value) {
 
 
         let gViz = svg
             .selectAll(".bubble")
             .data(processedData)
             .enter()
+            // .attr("translate", `transform(0, ${hPadding})`)
             .append("g")
             .attr("class", "bubble")
             .attr("transform", (d, i) => {
@@ -378,4 +427,5 @@ async function CreateBubbles(key, value) {
     }
 }
 
-CreateButtons();
+
+basicLayout()
